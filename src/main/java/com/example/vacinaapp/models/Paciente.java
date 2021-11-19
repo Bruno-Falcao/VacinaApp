f19ec48c744c2548f7b6ddebfce7cf4cebfb3da0
Bruno-Falcão <brunofalcao321@gmail.com>
2021-11-18 23:31:26 -0300

    removed imports

--- src/main/java/com/example/vacinaapp/models/Paciente.java
+++ src/main/java/com/example/vacinaapp/models/Paciente.java
@@ -1,9 +1,7 @@
 package com.example.vacinaapp.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
